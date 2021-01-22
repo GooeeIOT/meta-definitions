0c649a121e74c7501ebfd01e87dfde1ef36a8a95
lourdesmm90 <lourdesmm90@gmail.com>
2021-01-22 10:31:10 -0500

    adjust package setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 def long_description():
-    with open("README.md", "r", encoding="utf-8") as fr:
+    with open("README.md", "r") as fr:
         return fr.read()
 
 
@@ -16,11 +16,17 @@ setuptools.setup(
     version="0.0.1",
     author="Gooee",
     description="Meta Definitions used in the Gooee Cloud",
+    include_package_data=True,
     install_requires=requirements(),
     long_description=long_description(),
     long_description_content_type="text/markdown",
+    license="MIT",
     url="https://github.com/GooeeIOT/meta-definitions",
     packages=setuptools.find_packages(),
-    classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent",],
-    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3",
 )
